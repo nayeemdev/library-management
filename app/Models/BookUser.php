57ad009ec554529c741450745326bd237083ab7e
Nayeem Hossain <nayeem9812@gmail.com>
2020-10-30 20:06:27 +0600

    feat: Book Model Relationship created

--- app/Models/BookUser.php
+++ app/Models/BookUser.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class BookUser extends Model
 {
     use HasFactory;
+
+    protected $table = 'book_user';
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function book_copy()
+    {
+        return $this->belongsTo('App\Models\BookCopy');
+    }
+
+    public function loan_request()
+    {
+        return $this->belongsTo('App\Models\LoanRequest');
+    }
+
+    public function return_request()
+    {
+        return $this->belongsTo('App\Models\ReturnRequest');
+    }
 }
